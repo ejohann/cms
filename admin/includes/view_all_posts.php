d011a82563d20e8f9dbbd3b4225988803e8c1537
Johanne <johanne_lewis@hotmail.com>
2019-05-17 15:13:10 +0100

    Add option to clone post in admin dropdown

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -42,6 +42,7 @@
        <option value="published">Publish</option>
        <option value="draft">Draft</option>
        <option value="delete">Delete</option>
+       <option value="clone">Clone</option>
    </select>
   </div>
   <div class="col-xs-4"> 
