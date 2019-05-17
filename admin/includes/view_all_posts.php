865157ac6041f1ef280bb3d349994898613e9e86
Johanne <johanne_lewis@hotmail.com>
2019-05-17 15:36:23 +0100

    Order post by most recent in admin

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -91,7 +91,7 @@
   </thead>
   <tbody>
     <?php 
-      $query = "SELECT * FROM posts";
+      $query = "SELECT * FROM posts ORDER BY id DESC";
       $select_all_posts = mysqli_query($connection, $query);
       while($row = mysqli_fetch_assoc($select_all_posts))
         {
