ba6290ba916f4ef97854a9421f94f03727304fb0
Johanne <johanne_lewis@hotmail.com>
2019-05-17 09:08:37 +0100

    Add user firstname and lastname to dashboard

--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -14,12 +14,19 @@
   <ul class="nav navbar-right top-nav">
     <li><a href='../index.php'>Home</a></li>
     <li class="dropdown">
-      <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> John Smith <b class="caret"></b></a>
-      <ul class="dropdown-menu">
-        <li><a href="#"><i class="fa fa-fw fa-user"></i> Profile</a></li>
-        <li class="divider"></li>
-        <li><a href="../includes/logout.php"><i class="fa fa-fw fa-power-off"></i> Log Out</a></li>
-      </ul>
+      <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i>
+        <?php 
+          if(isset($_SESSION['user_firstname']))
+            {
+              echo " " . $_SESSION['user_firstname'] . " " . $_SESSION['user_lastname'];
+            } 
+        ?> 
+        <b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <li><a href="./profile.php"><i class="fa fa-fw fa-user"></i> Profile</a></li>
+          <li class="divider"></li>
+          <li><a href="../includes/logout.php"><i class="fa fa-fw fa-power-off"></i> Log Out</a></li>
+        </ul>
     </li>    
   </ul>
     
