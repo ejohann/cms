09d9bf0c127c8d88ae517d80ceeea14df1fd8c1f
Johanne <johanne_lewis@hotmail.com>
2019-05-17 14:54:33 +0100

    Add link to index page and post page to view related author post

--- author_post.php
+++ author_post.php
@@ -32,7 +32,7 @@
             $post_content = $row['post_content'];
       ?>
       <h2><a href="#"><?php echo $post_title; ?></a></h2>
-      <p class="lead">by <a href="author_post.php?author=<?php echo $post_author; ?>&post_id=<?php echo $post_id; ?>"><?php echo $post_author; ?></a></p>
+      <p class="lead">by <a href="index.php"><?php echo $post_author; ?></a></p>
       <p><span class="glyphicon glyphicon-time"></span><?php echo $post_date; ?></p>
       <hr>
       <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
--- index.php
+++ index.php
@@ -31,7 +31,7 @@
               {
       ?>
                 <h2><a href="post.php?post_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a></h2>
-                <p class="lead">by <a href="index.php"><?php echo $post_author; ?></a></p>
+                <p class="lead">by <a href="author_post.php?author=<?php echo $post_author; ?>&post_id=<?php echo $post_id; ?>"><?php echo $post_author; ?></a></p>
                 <p><span class="glyphicon glyphicon-time"></span><?php echo $post_date; ?></p>
                 <hr>
                 <a href="post.php?post_id=<?php echo $post_id; ?>"><img class="img-responsive" src="images/<?php echo $post_image; ?>" alt=""></img></a>
