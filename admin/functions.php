14ebf321cd286b0bf41d471a588b234572948753
Johanne <johanne_lewis@hotmail.com>
2019-05-19 13:34:00 +0100

    Made a function to escape variables when doing database queries

--- admin/functions.php
+++ admin/functions.php
@@ -1,6 +1,13 @@
 <?php
   
  
+function escape($string)
+ {
+    global $connection;
+    return mysqli_real_escape_string($connection, trim(strip_tags($string)));
+ }
+
+
 function users_online()
  {
     if(isset($_GET['online_users']))
