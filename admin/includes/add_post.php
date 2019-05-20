ac97d2d6b481d5e980ae54ab78921ca75e2acb86
Johanne <johanne_lewis@hotmail.com>
2019-05-20 17:46:20 +0100

    Add security feature to escape values in post request

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -1,14 +1,14 @@
 <?php
   if(isset($_POST['create_post']))
     {      
-      $post_title = $_POST['post_title'];
-      $post_category_id = $_POST['post_category_id'];
-      $post_author = $_POST['post_author'];
-      $post_status = $_POST['post_status'];
-      $post_image = $_FILES['post_image']['name'];
+      $post_title = escape($_POST['post_title']);
+      $post_category_id = escape($_POST['post_category_id']);
+      $post_author = escape($_POST['post_author']);
+      $post_status = escape($_POST['post_status']);
+      $post_image = escape($_FILES['post_image']['name']);
       $post_image_temp = $_FILES['post_image']['tmp_name'];
-      $post_tags = $_POST['post_tags'];
-      $post_content = $_POST['post_content'];
+      $post_tags = escape($_POST['post_tags']);
+      $post_content = escape($_POST['post_content']);
       $post_date = date('d-m-y');
       $post_comment_count = 0;
              
@@ -47,8 +47,6 @@
     </select>
   </div>
   
-  
-  
   <div class="form-group">
     <label for="post_author">Post Author</label>
     <select name="post_author" id="post_author">
@@ -66,14 +64,6 @@
       ?>     
     </select>
   </div>
-  
-  
- <!-- <div class="form-group">
-    <label for="post_author">Post Author</label>
-    <input type="text" class="form-control" name="post_author"></input>
-  </div> -->
-  
-  
   
   <div class="form-group">
     <label for="post_status">Post Status</label>
