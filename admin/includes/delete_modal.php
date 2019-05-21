40346319953a92def83cd5e951ea39be3ba213f9
Johanne <johanne_lewis@hotmail.com>
2019-05-21 13:52:09 +0100

    Customized text to confirm delete post and added a delete button

--- admin/includes/delete_modal.php
+++ admin/includes/delete_modal.php
@@ -9,13 +9,14 @@
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Modal Header</h4>
+        <h4 class="modal-title">Delete Post Dialog</h4>
       </div>
       <div class="modal-body">
-        <p>Some text in the modal.</p>
+        <p>Are you sure you want to delete this post?</p>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          <a href="" class="btn btn-danger modal_delete_link">Delete</a>
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
       </div>
     </div>
 
