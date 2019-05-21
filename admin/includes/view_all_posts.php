c550a5598eb0531d12aee9db39fa70ae39c54977
Johanne <johanne_lewis@hotmail.com>
2019-05-21 14:07:34 +0100

    Add javascript to target delete link

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -1,4 +1,10 @@
 <?php
+  include("delete_modal.php");
+
+?>
+ 
+
+<?php
   if(isset($_POST['checkBoxArray']))
    {
       foreach($_POST['checkBoxArray'] as $postIdValue)
@@ -145,7 +151,9 @@
           echo "<td>{$post_date}</td>";
           echo "<td><a href='../post.php?post_id={$post_id}'>View</a></td>";
           echo "<td><a href='posts.php?source=edit_post&post_id={$post_id}'>Edit</a></td>";
-          echo "<td><a onClick= \"javascript: return confirm('Are you sure you want to delete this post?'); \" href='posts.php?delete={$post_id}'>Delete</a></td>";
+          
+            echo "<td><a href='' class='delete_link'>Delete</a></td>";
+         // echo "<td><a onClick= \"javascript: return confirm('Are you sure you want to delete this post?'); \" href='posts.php?delete={$post_id}'>Delete</a></td>";
           echo "</tr>";
         }
     ?>                  
@@ -203,3 +211,16 @@
       }
     }
 ?>    
+
+
+<script>
+$(document).ready(function(){
+    
+    $(".delete_link").on('click', function(){
+        
+       alert("HELLO"); 
+    });
+    
+});
+    
+</script>
