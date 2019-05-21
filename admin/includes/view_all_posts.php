5da2dc02105fd5f6bbe4834e500270e2fc14e791
Johanne <johanne_lewis@hotmail.com>
2019-05-21 14:12:45 +0100

    Add code to get post id on delete link when clicked

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -152,7 +152,7 @@
           echo "<td><a href='../post.php?post_id={$post_id}'>View</a></td>";
           echo "<td><a href='posts.php?source=edit_post&post_id={$post_id}'>Edit</a></td>";
           
-            echo "<td><a href='' class='delete_link'>Delete</a></td>";
+            echo "<td><a rel='$post_id' href='' class='delete_link'>Delete</a></td>";
          // echo "<td><a onClick= \"javascript: return confirm('Are you sure you want to delete this post?'); \" href='posts.php?delete={$post_id}'>Delete</a></td>";
           echo "</tr>";
         }
@@ -217,8 +217,9 @@
 $(document).ready(function(){
     
     $(".delete_link").on('click', function(){
-        
-       alert("HELLO"); 
+       
+      var post_id = $(this).attr("rel");    
+       alert(post_id); 
     });
     
 });
