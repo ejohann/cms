f49b91e0e84561f3b1fe1d902f331fd7e328d6c9
Johanne <johanne_lewis@hotmail.com>
2019-05-22 11:56:25 +0100

    Show active link when on page

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,18 +1,42 @@
-<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+<?php
+session_start(); 
+ $registration_class = '';
+ $contact_class = '';
+ $home_class = '';
+ $home = 'index.php';
+ $contact = 'contact.php';
+ $registration = 'registration.php';
+ $page_name = basename($_SERVER['PHP_SELF']);
+ 
+ if($page_name == $registration)
+    {
+      $registration_class = 'active';  
+    }
+  else if($page_name == $contact)
+    {
+        $contact_class = 'active';  
+    }   
+  else if($page_name == $home)
+    {
+        $home_class = 'active';  
+    }              
+?>
+ 
+ <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
   <div class="container">
     <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+    <div class="navbar-header ">
+   <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="./index.php">Home</a>
     </div>
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
+           <li class='<?php echo $home_class; ?>'>  <a href="index.php">Home</a></li>
         <?php          
           $query = "SELECT * FROM categories";
           $select_all_categories = mysqli_query($connection, $query);
@@ -20,12 +44,19 @@
             {
               $category_title = $row['category_title'];
               $category_id = $row['id']; 
-              echo "<li><a href='category.php?category_id={$category_id}'>{$category_title}</a></li>";
+              
+               $category_class = '';
+             
+              if(isset($_GET['category_id']) && $_GET['category_id'] == $category_id)
+               {
+                 $category_class = 'active';   
+               }
+              echo "<li class='{$category_class}'><a href='category.php?category_id={$category_id}'>{$category_title}</a></li>";
             }
         ?>
-        <li><a href='contact.php'>Contact</a></li>  
+        <li  class='<?php echo $contact_class; ?>'><a href='contact.php'>Contact</a></li>  
         <?php  
-          session_start();   
+            
           if(isset($_SESSION['user_role']))
            {
               if($_SESSION['user_role'] == 'Admin')
@@ -40,7 +71,7 @@
            }
           else
           {
-             echo " <li><a href='registration.php'>User Registration</a></li>";   
+             echo " <li class='{$registration_class}'><a href='registration.php'>User Registration</a></li>";   
           }
         ?>
              
