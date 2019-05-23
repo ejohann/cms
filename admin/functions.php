5b6d93418168112f049dc435f663e3d79b68a903
Johanne <johanne_lewis@hotmail.com>
2019-05-23 16:10:36 +0100

    Create function to login users

--- admin/functions.php
+++ admin/functions.php
@@ -225,5 +225,37 @@ function is_admin($username = '')
       
    }
 
+  function login_user($username, $user_password)
+   {
+     $query = "SELECT * FROM users WHERE username = '{$username}' ";
+     $select_user_by_name = mysqli_query($connection, $query);
+     confirm_query($select_user_by_name);
+     while($row = mysqli_fetch_array($select_user_by_name))
+      {
+        $the_username = $row['username'];
+        $the_user_id = $row['id'];
+        $the_user_password = $row['user_password'];
+        $the_user_firstname = $row['user_firstname'];
+        $the_user_lastname = $row['user_lastname'];
+        $the_user_email = $row['user_email'];
+        $the_user_role = $row['user_role'];
+      }
+    
+      if(password_verify($user_password, $the_user_password))
+         {
+           // user login successful
+           $_SESSION['username'] = $the_username;
+           $_SESSION['user_firstname'] = $the_user_firstname;
+           $_SESSION['user_lastname'] = $the_user_lastname;
+           $_SESSION['user_email'] = $the_user_email;
+           $_SESSION['user_role'] = $the_user_role;
+           $_SESSION['user_id'] = $the_user_id;
+           header("Location: ../admin/index.php");
+          }
+         else
+          {      
+            header("Location: ../index.php");
+          }
+   }
 
 ?>
