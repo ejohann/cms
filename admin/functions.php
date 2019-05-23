d32869bf609f8efff0c34c5d54b7aa8a1b16d53a
Johanne <johanne_lewis@hotmail.com>
2019-05-23 20:10:06 +0100

    Call login function to login user,after successful registration

--- admin/functions.php
+++ admin/functions.php
@@ -222,6 +222,7 @@ function is_admin($username = '')
      $query .= "VALUES ('{$username}', '{$user_password}', '{$user_email}', '{$user_role}' )";    
      $register_user_query = mysqli_query($connection, $query);  
      confirm_query($register_user_query);
+     login_user($username, $user_password);
       
    }
 
