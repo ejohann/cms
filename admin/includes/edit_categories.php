fdc9f86d3bc134301fe5882fba7a456c44842bf8
Johanne <johanne_lewis@hotmail.com>
2019-05-24 12:27:02 +0100

    Use redirect function instead of header location

--- admin/includes/edit_categories.php
+++ admin/includes/edit_categories.php
@@ -33,7 +33,7 @@
               } 
              else
               {
-                  header("Location: categories.php");
+                redirect("categories.php");
               }
           }
       ?>                                   
