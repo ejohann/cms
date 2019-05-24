241faee8276cae4bbdaf7a62136b47e6388b1460
Johanne <johanne_lewis@hotmail.com>
2019-05-24 14:21:57 +0100

    Update author link to use rewrite rules

--- post.php
+++ post.php
@@ -53,10 +53,10 @@
                 $post_content = $row['post_content'];
       ?>
       <h2><a href="#"><?php echo $post_title; ?></a></h2>
-      <p class="lead">by <a href="author_post.php?author=<?php echo $post_author; ?>&post_id=<?php echo $post_id; ?>"><?php echo $post_author; ?></a></p>
+      <p class="lead">by <a href="/cms/authorpost/<?php echo $post_author; ?>/<?php echo $post_id; ?>"><?php echo $post_author; ?></a></p>
       <p><span class="glyphicon glyphicon-time"></span><?php echo $post_date; ?></p>
       <hr>
-      <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
+      <img class="img-responsive" src="/cms/images/<?php echo $post_image; ?>" alt="">
       <hr>
       <p><?php echo $post_content; ?></p>
       <hr>
