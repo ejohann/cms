62f6b7229d12c971b4a3f4ac6514422caf204eb6
Johanne <johanne_lewis@hotmail.com>
2019-05-24 12:03:56 +0100

    Convert select category query to prepared statement

--- admin/includes/edit_categories.php
+++ admin/includes/edit_categories.php
@@ -2,21 +2,38 @@
   <div class="form-group">
     <label for="category_title">Edit Category</label>
       <?php
-        $query_category = "SELECT * FROM categories WHERE id = $edit_category_id";
-        $select_category_by_id = mysqli_query($connection, $query_category);
-        confirm_query($select_category_by_id); 
-        $row = mysqli_fetch_array($select_category_by_id);
-        $category_title = $row['category_title'];
+      
+       $statement = mysqli_prepare($connection, "SELECT id, category_title FROM categories WHERE id = ? ");
+      
+       if(isset($statement))
+        {
+          mysqli_stmt_bind_param($statement, 'i', $edit_category_id);
+          mysqli_stmt_execute($statement);
+          mysqli_stmt_bind_result($statement, $category_id, $category_title);
+         
+        }
+      
+        while(mysqli_stmt_fetch($statement))
+         {
+           $edit_category_id = $category_id;
+           $category_title = $category_title;
+         }
+      //  $query_category = "SELECT * FROM categories WHERE id = $edit_category_id";
+       // $select_category_by_id = mysqli_query($connection, $query_category);
+    //    confirm_query($select_category_by_id); 
+      //  $row = mysqli_fetch_array($select_category_by_id);
+    //    $category_title = $row['category_title'];
         if(isset($_POST['edit']))
           {
             $the_category_title = escape($_POST['edit_category_title']);    
-            $query = "UPDATE categories SET category_title = '{$the_category_title}' WHERE id = '{$edit_category_id }' ";
+            $query = "UPDATE categories SET category_title = '{$the_category_title}' WHERE id = '{$edit_category_id}' ";
             $edit_query = mysqli_query($connection, $query);
-            header("Location: categories.php");
+           
             if(!$edit_query)
               {
                 die("Category Query Failed " . mysqli_error($connection));  
-              }         
+              } 
+             header("Location: categories.php");
           }
       ?>                                   
     <input class="form-control"  type="text" value=<?php echo $category_title; ?> name="edit_category_title"></input>
