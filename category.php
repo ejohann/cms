8d81dafafc2085321a3f0942f673531815f79309
Johanne <johanne_lewis@hotmail.com>
2019-05-24 13:55:27 +0100

    Updated image source for rewrite rule

--- category.php
+++ category.php
@@ -43,7 +43,7 @@
                  <p class="lead">by <a href="index.php"><?php echo $post_author; ?></a></p>
                  <p><span class="glyphicon glyphicon-time"></span><?php echo $post_date; ?></p>
                  <hr>
-                  <a href="post.php?post_id=<?php echo $post_id; ?>"><img class="img-responsive" src="images/<?php echo $post_image; ?>" alt=""></img></a>
+                  <a href="post.php?post_id=<?php echo $post_id; ?>"><img class="img-responsive" src="/cms/images/<?php echo $post_image; ?>" alt=""></img></a>
                  <hr>
                  <p><?php echo $post_content; ?></p>
                  <a class="btn btn-primary" href="post.php?post_id=<?php echo $post_id; ?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
