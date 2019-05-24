4a9f870fb631e74db72b864ad8400fc3fd5e23bc
Johanne <johanne_lewis@hotmail.com>
2019-05-24 14:23:57 +0100

    Update author link to use rewrite rules

--- category.php
+++ category.php
@@ -40,7 +40,7 @@
                  $post_content = "" . substr($post_content, 0, 100) . "...";
         ?>
                  <h2><a href="/cms/post/<?php echo $post_id; ?>"><?php echo $post_title; ?></a></h2>
-                 <p class="lead">by <a href="/cms/author_post.php?author=<?php echo $post_author; ?>&post_id=<?php echo $post_id; ?>"><?php echo $post_author; ?></a></p>
+                 <p class="lead">by <a href="/cms/authorpost/<?php echo $post_author; ?>/<?php echo $post_id; ?>"><?php echo $post_author; ?></a></p>
                  <p><span class="glyphicon glyphicon-time"></span><?php echo $post_date; ?></p>
                  <hr>
                   <a href="/cms/post/<?php echo $post_id; ?>"><img class="img-responsive" src="/cms/images/<?php echo $post_image; ?>" alt=""></img></a>
