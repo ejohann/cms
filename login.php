d97d434a1996e8acedc0646a01cfa5879fb1fb7e
Johanne <johanne_lewis@hotmail.com>
2019-05-25 13:21:26 +0100

    Move login file from includes folder to root directory

---
+++ login.php
@@ -0,0 +1,17 @@
+<?php session_start(); ?>
+<?php include "db.php"; ?>
+<?php include "../admin/functions.php"; ?>
+
+
+<?php 
+
+  if(isset($_POST['login']))
+   {
+     $username = escape($_POST['username']);
+     $password = escape($_POST['password']);
+     login_user($username, $password);
+      redirect("../admin");
+   }
+  
+
+?>
