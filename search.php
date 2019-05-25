86e6ef4e331cd0b22b51f85ae08e5d4cfa9b850b
Johanne <johanne_lewis@hotmail.com>
2019-05-25 16:41:25 +0100

    Tidy up code

--- search.php
+++ search.php
@@ -1,133 +1,84 @@
 <?php
-     include "./includes/db.php";
-
+   include "./includes/db.php";
    include "./includes/header.php";
    include "./admin/functions.php";
 ?>
 
-    <!-- Navigation -->
-     <?php
-        include "./includes/navigation.php";
+  <!-- Navigation -->
+  <?php
+    include "./includes/navigation.php";
+  ?>
+  <!-- PAGE BANNER HERE -->
+  <h1 class="page-header">Page Heading <small> Secondary Text</small></h1>
+
+  <!-- Page Content -->
+  <div class="container">
+    <div class="row">
+
+      <!-- Blog Entries Column -->
+      <div class="col-md-8">
+        <?php
+          if(isset($_POST['submit']))
+            {
+               $search = mysqli_real_escape_string($connection, $_POST['search']); 
+               $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%'";
+               $search_query = mysqli_query($connection, $query);
+               if(!$search_query)
+                 {
+                   die("Search query failed" . mysqli_error($connection));  
+                 }
+                else
+                 {
+                   $count = mysqli_num_rows($search_query);
+                   if($count === 0)
+                     {
+                       echo "<h1>No Results found for " . $search . "</h1>";    
+                     }
+                   else
+                     {
+                       while($row = mysqli_fetch_assoc($search_query))
+                         {
+                           $post_title = $row['post_title'];
+                           $post_author = $row['post_author'];
+                           $post_date = $row['post_date'];
+                           $post_image = $row['post_image'];
+                           $post_content = $row['post_content'];
+        ?>
+                           <h2><a href="#"><?php echo $post_title; ?></a></h2>
+                           <p class="lead"> by <a href="index.php"><?php echo $post_author; ?></a></p>
+                           <p><span class="glyphicon glyphicon-time"></span><?php echo $post_date; ?></p>
+                           <hr>
+                           <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
+                           <hr>
+                           <p><?php echo $post_content; ?></p>
+                           <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                           <hr>
+        <?php
+                         }
+                     }
+                 }
+            }
+          else
+            {
+              redirect("/cms");
+            }
+        ?>
+
+        <!-- Pager -->
+        <ul class="pager">
+          <li class="previous"><a href="#">&larr; Older</a></li>
+          <li class="next"><a href="#">Newer &rarr;</a></li>
+        </ul>
+
+      </div> <!-- END BLOG ENTRIES -->
+
+      <!-- Blog Sidebar Widgets Column -->
+      <?php
+         include "./includes/sidebar.php";
       ?>
-    <!-- Page Content -->
-    <div class="container">
 
-        <div class="row">
-
-            <!-- Blog Entries Column -->
-            <div class="col-md-8">
-                
-                <?php
-                
-                
-                if(isset($_POST['submit']))
-                   {
-                     $search = mysqli_real_escape_string($connection, $_POST['search']); 
-                     
-                     $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%'";
-                     $search_query = mysqli_query($connection, $query);
-                      
-                     if(!$search_query)
-                      {
-                        die("Search query failed" . mysqli_error($connection));  
-                      }
-                     else
-                      {
-                         $count = mysqli_num_rows($search_query);
-                        
-                         if($count === 0)
-                          {
-                             echo "<h1>No Results found for " . $search . "</h1>";    
-                          }
-                         else
-                          {
-                            while($row = mysqli_fetch_assoc($search_query))
-                             {
-                               $post_title = $row['post_title'];
-                               $post_author = $row['post_author'];
-                               $post_date = $row['post_date'];
-                               $post_image = $row['post_image'];
-                                $post_content = $row['post_content'];
-                                
-                                ?>
-                                
-                                <h2>
-                    <a href="#"><?php echo $post_title; ?></a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php"><?php echo $post_author; ?></a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span><?php echo $post_date; ?></p>
-                <hr>
-                <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
-                <hr>
-                <p><?php echo $post_content; ?></p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-               
-                
-                  <hr>
-                                    
-                                
-                                
-                                <?php
-                              }
-                    
-                          }
-                         
-                      }
-                    
-                      
-                   }
-                  else
-                   {
-                      redirect("/cms");
-                   }
-                
-                
-                
-                     
-                   
-                       
-
-                      //    }
-                ?>
-
-                <h1 class="page-header">
-                    Page Heading
-                    <small>Secondary Text</small>
-                </h1>
-
-               
-               
-                <!-- First Blog Post -->
-             
- 
-
-                
-
-                <!-- Pager -->
-                <ul class="pager">
-                    <li class="previous">
-                        <a href="#">&larr; Older</a>
-                    </li>
-                    <li class="next">
-                        <a href="#">Newer &rarr;</a>
-                    </li>
-                </ul>
-
-            </div>
-
-            <!-- Blog Sidebar Widgets Column -->
-          
-             <?php
-                 include "./includes/sidebar.php";
-            ?>
-
-        </div>
-        <!-- /.row -->
-
-        <hr>
+    </div><!-- /.row -->
+    <hr>
 
 <?php
    include "./includes/footer.php";
