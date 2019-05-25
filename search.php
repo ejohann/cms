ad22de95dbff866560a40fda540094c1f603abae
Johanne <johanne_lewis@hotmail.com>
2019-05-25 16:53:41 +0100

    Update post links

--- search.php
+++ search.php
@@ -21,7 +21,7 @@
           if(isset($_POST['submit']))
             {
                $search = mysqli_real_escape_string($connection, $_POST['search']); 
-               $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%'";
+               $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%' AND post_status = 'published' ";
                $search_query = mysqli_query($connection, $query);
                if(!$search_query)
                  {
@@ -42,16 +42,17 @@
                            $post_author = $row['post_author'];
                            $post_date = $row['post_date'];
                            $post_image = $row['post_image'];
-                           $post_content = $row['post_content'];
+                           $post_content = "" . substr($row['post_content'], 0, 100) . "...";
+                           $post_id = $row['id'];
         ?>
-                           <h2><a href="#"><?php echo $post_title; ?></a></h2>
-                           <p class="lead"> by <a href="index.php"><?php echo $post_author; ?></a></p>
+                           <h2><a href="/cms/post/<?php echo $post_id; ?>"><?php echo $post_title; ?></a></h2>
+                           <p class="lead"> by <a href="/cms/authorpost/<?php echo $post_author; ?>/<?php echo $post_id; ?>"><?php echo $post_author; ?></a></p>
                            <p><span class="glyphicon glyphicon-time"></span><?php echo $post_date; ?></p>
                            <hr>
-                           <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
+                           <a href="/cms/post/<?php echo $post_id; ?>"><img class="img-responsive" src="/cms/images/<?php echo $post_image; ?>" alt=""></a>
                            <hr>
                            <p><?php echo $post_content; ?></p>
-                           <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                           <a class="btn btn-primary" href="/cms/post/<?php echo $post_id; ?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
                            <hr>
         <?php
                          }
