70bba7771dd81480eb23701fdb90f181427e4b7e
Johanne <johanne_lewis@hotmail.com>
2019-05-26 13:01:00 +0100

    Create configuration class for PHPmailer with Mailtrap credentials

---
+++ classes/config.php
@@ -0,0 +1,13 @@
+<?php
+
+class Config
+ {
+   const SMTP_HOST = "smtp.mailtrap.io";
+   const SMTP_PORT = 2525;
+   const SMTP_USER = '31453cb6db0d3a';
+   const SMTP_password = '7cb16399d24468';
+    
+    
+ }
+
+?>
