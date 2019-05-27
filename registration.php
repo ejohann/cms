cf40505d856e7e5d03a845837b98cc499d0f2d45
Johanne <johanne_lewis@hotmail.com>
2019-05-27 10:00:53 +0100

    Require pusher library via composer

--- registration.php
+++ registration.php
@@ -1,6 +1,7 @@
 <?php  include "includes/db.php"; ?>
 <?php  include "includes/header.php"; ?>
 <?php  include "admin/functions.php"; ?>
+<?php require "vendor/autoload.php"; ?>
 
 <!-- Navigation -->
 <?php  include "includes/navigation.php"; ?>
