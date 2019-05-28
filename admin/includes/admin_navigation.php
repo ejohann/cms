a497294760ea323517723bb28dbde3d4920614e4
Johanne <johanne_lewis@hotmail.com>
2019-05-28 18:04:35 +0100

    Made link for user dashboard

--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -35,7 +35,8 @@
   <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
   <div class="collapse navbar-collapse navbar-ex1-collapse">
     <ul class="nav navbar-nav side-nav">
-      <li><a href="./index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a></li>
+      <li><a href="./index.php"><i class="fa fa-fw fa-dashboard"></i> My Data</a></li>
+       <li><a href="./dashboard.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a></li>
       <li><a href="javascript:;" data-toggle="collapse" data-target="#posts_dropdown"><i class="fa fa-fw fa-arrows-v"></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
         <ul id="posts_dropdown" class="collapse">
           <li><a href="./posts.php">View All Posts</a></li>
