3f23e18f81e86dddda063af0751e08897ea57353
Johanne <johanne_lewis@hotmail.com>
2019-05-29 11:23:08 +0100

    Made boxes bigger

--- admin/index.php
+++ admin/index.php
@@ -18,7 +18,7 @@
       </div> <!-- /.row -->
                        
       <div class="row">
-        <div class="col-lg-3 col-md-6">
+        <div class="col-lg-4 col-md-6">
           <div class="panel panel-primary">
             <div class="panel-heading">
               <div class="row">
@@ -44,7 +44,7 @@
           </div>
         </div>
       
-      <div class="col-lg-3 col-md-6">
+      <div class="col-lg-4 col-md-6">
         <div class="panel panel-green">
           <div class="panel-heading">
             <div class="row">
@@ -70,7 +70,7 @@
         </div>
       </div>
 
-      <div class="col-lg-3 col-md-6">
+      <div class="col-lg-4 col-md-6">
         <div class="panel panel-red">
           <div class="panel-heading">
             <div class="row">
