b9015531e7819d345c8a0d5355e3c40e2826be0e
Johanne <johanne_lewis@hotmail.com>
2019-05-29 12:16:53 +0100

    Tidy up code

--- admin/functions.php
+++ admin/functions.php
@@ -1,88 +1,77 @@
 <?php
 
 
+/**************************************************/
+          /* GENERAL FUNCTIONS */
+/**************************************************/
 
-
-
+// displays a default image if no image attached to post
 function image_placeholder($image=null)
  {
    if(!$image)
     {
        return 'Tulips.jpg';
-   }
-  else
-   {
+    }
+   else
+    {
       return $image;
-  }
+   }
  }
 
-
-
-
+// trims a value from post or get request
 function escape($string)
  {
     global $connection;
     return mysqli_real_escape_string($connection, trim($string));
  }
 
-
-function users_online()
- {
-    if(isset($_GET['online_users']))
-    {
-         global $connection;
-        
-        if(!$connection)
-         {
-            session_start();
-            include("../includes/db.php");
-             $minute = 60;
-  $hour = 60;
-  $day = 24;
-  $session_id = session_id();
-  $session_time = time();
-  $time_out_in_seconds = $minute;
-  $time_out = $session_time - $time_out_in_seconds;
- 
-  $query = "SELECT * FROM users_online WHERE session = '{$session_id}' ";
-  $online_users_query = mysqli_query($connection, $query);
-  confirm_query($online_users_query);
-  $session_count = mysqli_num_rows($online_users_query);
-
-  if($session_count == NULL)
-    {
-      $query = "INSERT INTO users_online(session, time) VALUES ('{$session_id}', '{$session_time}')";
-      $add_session_query = mysqli_query($connection, $query);
-      confirm_query($add_session_query);
-   }
- else
+// confirms a db query has been successful
+function confirm_query($query_result)
   {
-      $query = "UPDATE users_online SET time = '{$session_time}' WHERE session = '{$session_id}' ";
-      $update_session_query = mysqli_query($connection, $query);
-      confirm_query($update_session_query);
+    global $connection;
+    if(!$query_result)
+      {
+        die("Post Query Failed " . mysqli_error($connection));   
+      }  
   }
-  
- $query = "SELECT * FROM users_online WHERE time > '{$time_out}'";
- $users_online_query = mysqli_query($connection, $query);
- confirm_query($users_online_query);
- 
-  $users_online_count = mysqli_num_rows($users_online_query);
-  echo $users_online_count;
-        }
-    }
-}
 
-users_online();
+// redirects a user to a page
+function redirect($location)
+  {
+     header("Location: " . $location . ""); 
+     exit;
+  }
 
-  function confirm_query($query_result)
-   {
-     global $connection;
-     if(!$query_result)
+ // this method has to be troubleshooted
+ function if_it_is_method($method = mull)
+  { 
+    if($_SERVER['REQUEST_METHOD'] == strtoupper($method))
+     {
+       return true;    
+     }
+    return false;  
+  }
+
+ // this method has to be troubleshooted
+ function check_if_user_logged_in_and_redirect($redirectLocation)
+    {
+      if(is_logged_in())
        {
-         die("Post Query Failed " . mysqli_error($connection));   
-       }  
-   }
+         redirect($redirectLocation);   
+       }
+       
+      return false;
+    }
+  
 
+/************************************************/
+          /* END GENERAL FUNCTIONS */
+/**************************************************/
+
+
+/************************************************/
+          /* ADMIN FUNCTIONS */
+/**************************************************/
 
 function add_categories_form()
   {
@@ -97,8 +86,7 @@ function add_categories_form()
         </div>
       </form> 
         ";
-   }
-  
+   } 
 
 function insert_categories()
   {
@@ -127,7 +115,6 @@ function insert_categories()
        }                      
    }
 
-
 function delete_categories()
   {
     global $connection; 
@@ -153,6 +140,63 @@ function delete_categories()
            }
       }
   }
+
+/************************************************/
+          /* END ADMIN FUNCTIONS */
+/**************************************************/
+
+
+/************************************************/
+  /* DISPLAY ONLINE USERS IN ADMIN FUNCTIONS */
+/**************************************************/
+
+// Display users online - uses javascript
+function users_online()
+ {
+   if(isset($_GET['online_users']))
+    {
+      global $connection;
+      if(!$connection)
+        {
+          session_start();
+          include("../includes/db.php");
+          $minute = 60;
+          $hour = 60;
+          $day = 24;
+          $session_id = session_id();
+          $session_time = time();
+          $time_out_in_seconds = $minute;
+          $time_out = $session_time - $time_out_in_seconds;
+          $query = "SELECT * FROM users_online WHERE session = '{$session_id}' ";
+          $online_users_query = mysqli_query($connection, $query);
+          confirm_query($online_users_query);
+          $session_count = mysqli_num_rows($online_users_query);
+          if($session_count == NULL)
+           {
+             $query = "INSERT INTO users_online(session, time) VALUES ('{$session_id}', '{$session_time}')";
+             $add_session_query = mysqli_query($connection, $query);
+             confirm_query($add_session_query);
+           }
+          else
+           {
+             $query = "UPDATE users_online SET time = '{$session_time}' WHERE session = '{$session_id}' ";
+             $update_session_query = mysqli_query($connection, $query);
+             confirm_query($update_session_query);
+           }
+          $query = "SELECT * FROM users_online WHERE time > '{$time_out}'";
+          $users_online_query = mysqli_query($connection, $query);
+          confirm_query($users_online_query);
+          $users_online_count = mysqli_num_rows($users_online_query);
+          echo $users_online_count;
+        }
+    }
+ }
+users_online();
+
+/************************************************/
+/* END DISPLAY ONLINE USERS IN ADMIN FUNCTIONS */
+/**************************************************/
+  
 
 /**************************************************/
           /* GET DATA FOR DISPLAYING CHARTS */
@@ -232,6 +276,13 @@ function check_status_by_user($table, $column, $status)
           /* END GET DATA FOR DISPLAYING CHARTS */
 /**************************************************/
 
+
+/**************************************************/
+          /* USER LOGIN FUNCTIONS */
+/**************************************************/
+
+
+ // returns the username of the logged in user
 function get_username()
  {
    if(isset($_SESSION['username']))
@@ -243,7 +294,6 @@ function get_username()
       return null;   
     }
  }
-
 
 // check if user is admin and logged in
 function is_admin($username = '')
@@ -273,56 +323,6 @@ function is_admin($username = '')
      }
    return false;
  }
- 
-
-
- function username_exists($username)
-  {
-     global $connection;
-     $query = "SELECT username FROM users WHERE username = '$username'";
-     $results = mysqli_query($connection, $query);
-     confirm_query($results);
-     if(mysqli_num_rows($results) == 0)
-      {
-         return true;
-      }
-     else
-      {
-        return false;
-      }
-  }
-
- function email_exists($email)
-  {
-     global $connection;
-     $query = "SELECT user_email FROM users WHERE user_email = '$email'";
-     $results = mysqli_query($connection, $query);
-     confirm_query($results);
-     if(mysqli_num_rows($results) == 0)
-      {
-         return true;
-      }
-     else
-      {
-        return false;
-      }
-  }
-
- function redirect($location)
-  {
-     header("Location: " . $location . ""); 
-     exit;
-  }
-
- function if_it_is_method($method = mull)
-  { 
-    if($_SERVER['REQUEST_METHOD'] == strtoupper($method))
-     {
-       return true;    
-     }
-    return false;  
-  }
-
 
  //check if a user is logged in
  function logged_in_user_id()
@@ -344,6 +344,116 @@ function is_admin($username = '')
      }
      return false;
   }
+ 
+ // check if a user is logged in
+ function is_logged_in()
+  {
+     if(isset($_SESSION['user_role'])  && $_SESSION['user_role'] != null)
+      {
+        return true;   
+      }
+     
+     return false;
+  }
+
+//login a user
+function login_user($username, $user_password)
+  {
+     global $connection;  
+
+     $query = "SELECT * FROM users WHERE username = '{$username}' ";
+     $select_user_by_name = mysqli_query($connection, $query);
+     confirm_query($select_user_by_name);
+     while($row = mysqli_fetch_array($select_user_by_name))
+      {
+        $the_username = $row['username'];
+        $the_user_id = $row['id'];
+        $the_user_password = $row['user_password'];
+        $the_user_firstname = $row['user_firstname'];
+        $the_user_lastname = $row['user_lastname'];
+        $the_user_email = $row['user_email'];
+        $the_user_role = $row['user_role'];
+        if(password_verify($user_password, $the_user_password))
+         {
+           // user login successful / create sessions
+           $_SESSION['username'] = $the_username;
+           $_SESSION['user_firstname'] = $the_user_firstname;
+           $_SESSION['user_lastname'] = $the_user_lastname;
+           $_SESSION['user_email'] = $the_user_email;
+           $_SESSION['user_role'] = $the_user_role;
+           $_SESSION['user_id'] = $the_user_id;
+           redirect("/cms/admin");      
+          }   
+        else
+         {
+            redirect("/cms/login");
+            return false;
+        }
+      }  
+   }
+
+/**************************************************/
+          /* END USER LOGIN FUNCTIONS */
+/**************************************************/
+
+
+/**************************************************/
+          /* USER REGISTRATION FUNCTIONS */
+/**************************************************/
+
+// check if a username exists
+function username_exists($username)
+  {
+     global $connection;
+     $query = "SELECT username FROM users WHERE username = '$username'";
+     $results = mysqli_query($connection, $query);
+     confirm_query($results);
+     if(mysqli_num_rows($results) == 0)
+      {
+         return true;
+      }
+     else
+      {
+        return false;
+      }
+  }
+
+ // check if an email address exists
+function email_exists($email)
+  {
+     global $connection;
+     $query = "SELECT user_email FROM users WHERE user_email = '$email'";
+     $results = mysqli_query($connection, $query);
+     confirm_query($results);
+     if(mysqli_num_rows($results) == 0)
+      {
+         return true;
+      }
+     else
+      {
+        return false;
+      }
+  }
+
+ // registers a user
+ function register_user($username, $user_password, $user_email, $user_role)
+   {
+     global $connection;
+     $user_hash_password = password_hash($user_password, PASSWORD_DEFAULT, array('cost' => 10));
+     $query = "INSERT INTO users (username, user_password, user_email, user_role) ";
+     $query .= "VALUES ('{$username}', '{$user_hash_password}', '{$user_email}', '{$user_role}' )";    
+     $register_user_query = mysqli_query($connection, $query);  
+     confirm_query($register_user_query);   
+   }
+
+/**************************************************/
+          /* END USER REGISTRATION FUNCTIONS */
+/**************************************************/
+
+
+/**************************************************/
+          /* POST LIKE FUNCTIONS */
+/**************************************************/
 
 // check if a user liked a post
 function user_liked_post($post_id = null)
@@ -366,7 +476,6 @@ function user_liked_post($post_id = null)
     }
    return false;  
   } 
-
  
  // get number of post likes
  function get_post_likes($post_id = null)
@@ -381,75 +490,10 @@ function user_liked_post($post_id = null)
      return $total_post_likes;
   }
 
- // check if a user is logged in
- function is_logged_in()
-  {
-     if(isset($_SESSION['user_role'])  && $_SESSION['user_role'] != null)
-      {
-        return true;   
-      }
-     
-     return false;
-  }
+/**************************************************/
+          /* END POST LIKE FUNCTIONS */
+/**************************************************/
+ 
 
-   function check_if_user_logged_in_and_redirect($redirectLocation)
-    {
-      if(is_logged_in())
-       {
-         redirect($redirectLocation);   
-       }
-       
-      return false;
-    }
   
-
-
-  function register_user($username, $user_password, $user_email, $user_role)
-   {
-     global $connection;
-     $user_hash_password = password_hash($user_password, PASSWORD_DEFAULT, array('cost' => 10));
-     $query = "INSERT INTO users (username, user_password, user_email, user_role) ";
-     $query .= "VALUES ('{$username}', '{$user_hash_password}', '{$user_email}', '{$user_role}' )";    
-     $register_user_query = mysqli_query($connection, $query);  
-     confirm_query($register_user_query);   
-   }
-
-  function login_user($username, $user_password)
-   {
-     global $connection;  
-
-     $query = "SELECT * FROM users WHERE username = '{$username}' ";
-     $select_user_by_name = mysqli_query($connection, $query);
-     confirm_query($select_user_by_name);
-     while($row = mysqli_fetch_array($select_user_by_name))
-      {
-        $the_username = $row['username'];
-        $the_user_id = $row['id'];
-        $the_user_password = $row['user_password'];
-        $the_user_firstname = $row['user_firstname'];
-        $the_user_lastname = $row['user_lastname'];
-        $the_user_email = $row['user_email'];
-        $the_user_role = $row['user_role'];
-        if(password_verify($user_password, $the_user_password))
-         {
-           // user login successful
-           $_SESSION['username'] = $the_username;
-           $_SESSION['user_firstname'] = $the_user_firstname;
-           $_SESSION['user_lastname'] = $the_user_lastname;
-           $_SESSION['user_email'] = $the_user_email;
-           $_SESSION['user_role'] = $the_user_role;
-           $_SESSION['user_id'] = $the_user_id;
-            redirect("/cms/admin");
-           
-          }   
-        else
-         {
-            redirect("/cms/login");
-            return false;
-        }
-      }
-    
-     
-   }
-
 ?>
