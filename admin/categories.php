2f4958e2ef90de034cbbf439887248460926e8aa
Johanne <johanne_lewis@hotmail.com>
2019-05-29 12:44:19 +0100

    Only allow admin to edit categories

--- admin/categories.php
+++ admin/categories.php
@@ -10,27 +10,20 @@
          <!-- Page Heading -->
          <div class="row">
            <div class="col-lg-12">
-             <h1 class="page-header">Categories <small><?php echo $_SESSION['username']; ?></small></h1>       <div class="col-xs-6">
+             <h1 class="page-header">Categories <small><?php echo get_username(); ?></small></h1>       <div class="col-xs-6">
     
                <?php
                   // UPDATE CATEGORY FORM/QUERY
                   if(isset($_GET['edit']))
                     { 
-                      if(isset($_SESSION['user_role']))
-                        {
-                          if($_SESSION['user_role'] == "Admin")
-                            {
-                              $edit_category_id = escape($_GET['edit']); 
-                              include "includes/edit_categories.php";
-                            }
-                           else
-                            {    
-                               header("Location: index.php");
-                            }
+                      if(is_admin(get_username()))
+                        { //only allow admin to edit categories
+                          $edit_category_id = escape($_GET['edit']); 
+                          include "includes/edit_categories.php";
                         }
                        else
                         {
-                           header("Location: ../index.php");
+                           redirect("/cms/admin/");
                         }
                     }
                    else
