00c2e1329dec12652f1dbd0790181e5caa1bf053
Johanne <johanne_lewis@hotmail.com>
2019-05-30 11:02:30 +0100

    If user is admin show select all check box

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -71,24 +71,27 @@
 
  <table class="table table-bordered table-hover">
   <?php if($is_admin) : ?>
-  <div id="bulkOptionContainer" class="col-xs-4" style="padding: 0px;">
-   <select class="form-control" name="bulk_options" id="">
-       <option value="">Select Options</option>
-       <option value="published">Publish</option>
-       <option value="draft">Draft</option>
-       <option value="delete">Delete</option>
-       <option value="clone">Clone</option>
-   </select>
-  </div>
-  <div class="col-xs-4"> 
-     <input type="submit" name="submit" class="btn btn-success" value="Apply"></input>
-     <a class="btn btn-primary" href="posts.php?source=add_post">Add New</a>
-  </div>  
+    <div id="bulkOptionContainer" class="col-xs-4" style="padding: 0px;">
+      <select class="form-control" name="bulk_options" id="">
+        <option value="">Select Options</option>
+        <option value="published">Publish</option>
+        <option value="draft">Draft</option>
+        <option value="delete">Delete</option>
+        <option value="clone">Clone</option>
+      </select>
+    </div>
+    <div class="col-xs-4"> 
+      <input type="submit" name="submit" class="btn btn-success" value="Apply"></input>
+      <a class="btn btn-primary" href="posts.php?source=add_post">Add New</a>
+    </div>  
   <?php else : ?>
   <?php endif; ?>
   <thead>
     <tr>
-      <th><input id="selectAllBoxes" type="checkbox"></th>
+      <?php if($is_admin) : ?>
+        <th><input id="selectAllBoxes" type="checkbox"></th>
+      <?php else : ?>
+      <?php endif; ?>
       <th>ID</th>
       <th>Author</th>
       <th>Title</th>
