6748d7707a515c6d76aa0d469ba03b00b500dcab
Johanne <johanne_lewis@hotmail.com>
2019-05-31 10:24:36 +0100

    Convert select post query to prepared statement

--- index.php
+++ index.php
@@ -56,17 +56,17 @@
                  $page_1 = ($page * $post_per_page) - $post_per_page;     
               }
             
-             $query = "SELECT * FROM posts ORDER BY id DESC LIMIT $page_1, $post_per_page";
-             $select_all_posts = mysqli_query($connection, $query);
-             while($row = mysqli_fetch_assoc($select_all_posts))
-              {
-                $post_id = $row['id'];
-                $post_title = $row['post_title'];
-                $post_author = $row['post_author'];
-                $post_date = $row['post_date'];
-                $post_image = $row['post_image'];
-                $post_content = "" . substr($row['post_content'], 0, 100) . "...";
-                $post_status = $row['post_status'];                           
+             $post_by_page = mysqli_prepare($connection, "SELECT id, post_title, post_author, post_date, post_image, post_content, post_status FROM posts WHERE post_status = ? ORDER BY id DESC LIMIT ".$page_1. " , " .$post_per_page. " ");
+             $published = 'published';
+             mysqli_stmt_bind_param($post_by_page, 's', $published);
+             mysqli_stmt_execute($post_by_page);
+             confirm_query($post_by_page);
+             mysqli_stmt_store_result($post_by_page);
+             mysqli_stmt_bind_result($post_by_page, $post_id, $post_title, $post_author, $post_date, $post_image, $post_content, $post_status);
+            
+             while(mysqli_stmt_fetch($post_by_page))
+              {   
+                $post_content = "" . substr($post_content, 0, 100) . "...";
         ?>
                 <h2><a href="/cms/post/<?php echo $post_id; ?>"><?php echo $post_title; ?></a></h2>
                 <p class="lead">by <a href="/cms/authorpost/<?php echo $post_author; ?>/<?php echo $post_id; ?>"><?php echo $post_author; ?></a></p>
@@ -78,7 +78,8 @@
                 <a class="btn btn-primary" href="/cms/post/<?php echo $post_id; ?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
                 <hr>
             <?php          
-              }    
+              }
+             mysqli_stmt_close($post_by_page);
             ?>
 
       <!-- Pager -->
