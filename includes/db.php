92f59311e9c35c74da34fd6627f530c68c6d1c77
Johanne <johanne_lewis@hotmail.com>
2019-05-31 16:04:29 +0100

    Use environment variables

--- includes/db.php
+++ includes/db.php
@@ -1,30 +1,20 @@
 <?php
+   require "vendor/autoload.php";
+   $dotenv = Dotenv\Dotenv::create(__DIR__.'/../');
+   $dotenv->load();
 
-    $db['db_host'] = "localhost";
-    $db['db_user'] = "root"; 
-    $db['db_password'] = ""; 
-    $db['db_name'] = "cms"; 
+   $connection = mysqli_connect(getenv('DB_HOST'), getenv('DB_USER'), getenv('DB_PASSWORD'), getenv('DB_NAME'));
+   $query = "SET NAMES utf8";
+   mysqli_query($connection, $query);
 
-   // converts keys to constants
-   foreach($db as $key => $value)
-    {
-      define(strtoupper($key), $value); 
-         
-    }
-
-
-    $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-    $query = "SET NAMES utf8";
-    mysqli_query($connection, $query);
-
-     if($connection)
-      {
+   if($connection)
+     {
          // echo "we are connected";
-      }
-     else
-      {
-         echo "Connection failed: ".  mysqli_error($connection); 
-      }
+     }
+    else
+     {
+        echo "Connection failed: ".  mysqli_error($connection); 
+     }
  
 
 ?>
