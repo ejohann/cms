e57d7c0ba823e42588fe47afb2b394437dd23d95
Johanne <johanne_lewis@hotmail.com>
2019-05-31 17:17:58 +0100

    Display page title and description dynamically

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -17,9 +17,11 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="description" content="">
-  <meta name="author" content="">
-  <title>CMS Admin</title>
+  <?php include("title_selector.php");
+   echo "<meta name='{$description}' content=''>";
+   echo "<meta name='author' content=''>";
+   echo "<title>{$title}</title>";
+  ?>
   <!-- Bootstrap Core CSS -->
   <link href="css/bootstrap.min.css" rel="stylesheet">
   <!-- Custom CSS -->
